d8ada1a51ed4467c75aca5d3ccadb475ccdd3dc5
pedro420b <badrxxxpedro@gmail.com>
2024-07-18 14:25:23 +0100

    v00001

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -11,22 +14,78 @@ class MainScreen extends StatelessWidget {
         child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  width: 50,
-                  height: 50,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.yellow
-                  ),
-                ),
-                Column(
+                Row(
                   children: [
-                    
+                    Stack(
+                      children: [
+                        Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.yellow
+                        ),
+                      )  
+                      ,
+                      const SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: Icon(
+                              CupertinoIcons.person_fill,
+                              color: Color.fromARGB(255, 70, 56, 230),
+                              size: 30,
+                              
+                            ),
+                      ),                  ]
+                    ),
+                         
+                         const SizedBox(width: 15),
+                         Column(
+                      children: [
+                        Text("welcome",style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Theme.of(context).colorScheme.outline
+                        ),
+                        ),
+                        Text("joe Daoe",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ))
+                      ],
+                    )
                   ],
-                )
+                ),
+                IconButton(onPressed: (){}, icon: const Icon(CupertinoIcons.settings))
               ],
+
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width/2,
+              decoration:  BoxDecoration(
+                gradient: LinearGradient(
+              colors: [
+                Theme.of(context).colorScheme.primary,
+                Theme.of(context).colorScheme.secondary,
+                Theme.of(context).colorScheme.tertiary,
+
+              ],
+              transform: const GradientRotation(pi/4),
+              ),
+                borderRadius: BorderRadius.circular(25)
+
+
+              ),
+
+
             )
+
           ],
         ),
       ),
